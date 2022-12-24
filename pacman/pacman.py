95046c2c9bfcba1c397db621163d6707ba317a0d
Lucas Felix Aquino <ext.lucas.aquino@farfetch.com>
2022-12-24 21:32:21 +0000

    bolinha amarela

---
+++ pacman/pacman.py
@@ -0,0 +1,16 @@
+import pygame
+
+AMARELO = (255, 255, 0)
+
+pygame.init()
+
+tela = pygame.display.set_mode((640, 480), 0)
+
+while True:
+
+    pygame.draw.circle(tela, AMARELO, (320, 240), 50, 0)
+    pygame.display.update()
+
+    for e in pygame.event.get():
+        if e.type == pygame.QUIT:
+            exit()
