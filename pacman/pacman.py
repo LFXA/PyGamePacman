8eb032478c755293b63d7fc82cf825a8b1c219ac
Lucas Felix Aquino <ext.lucas.aquino@farfetch.com>
2023-01-02 23:57:44 +0000

    pacman no cenario

--- pacman/pacman.py
+++ pacman/pacman.py
@@ -4,17 +4,19 @@ AMARELO = (255, 255, 0)
 PRETO = (0, 0, 0)
 BRANCO = (255, 255, 255)
 AZUL = (0, 0, 255)
-VELOCIDADE = 0.1
+VELOCIDADE = 1
 
 RAIO = 30
 pygame.init()
 
-tela = pygame.display.set_mode((600, 600), 0)
+tela = pygame.display.set_mode((800, 600), 0)
 
 
 class Cenario:
-    def __init__(self, tamanho):
+    def __init__(self, tamanho, pac):
         self.tamanho = tamanho
+        self.pacman = pac
+        self.pontos = 0
         self.matriz = [
             [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
             [2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2],
@@ -47,20 +49,35 @@ class Cenario:
             [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2]
         ]
 
-    def pintar_coluna(self, tela, numero_linha, linha):
+    def pintar_linha(self, tela, numero_linha, linha):
         for numero_coluna, coluna in enumerate(linha):
             x = numero_coluna * self.tamanho
             y = numero_linha * self.tamanho
-            half =(self.tamanho //2)
+            half =(self.tamanho // 2)
             cor = PRETO
             if coluna == 2:
                 cor = AZUL
             pygame.draw.rect(tela, cor, (x, y, self.tamanho, self.tamanho), 0)
             if coluna == 1:
                 pygame.draw.circle(tela, BRANCO, (x + half, y + half), self.tamanho//10, 0)
+
     def pintar(self, tela):
         for numero_linha, linha in enumerate(self.matriz):
-            self.pintar_coluna(tela, numero_linha, linha)
+            self.pintar_linha(tela, numero_linha, linha)
+
+    def calcular_regras(self):
+        col = self.pacman.coluna_intencao
+        lin = self.pacman.linha_intencao
+        if 0 <= col < 28 and 0 <= lin < 29:
+            if self.matriz[lin][col] != 2:
+                self.pacman.aceitar_movimento()
+                if self.matriz[lin][col] == 1:
+                    self.pontos += 1
+                    self.matriz[lin][col] = 0
+            else:
+                self.pacman.vel_x = 0
+                self.pacman.vel_y = 0
+
 class Pacman:
     def __init__(self, tamanho):
         self.coluna = 1
@@ -71,17 +88,14 @@ class Pacman:
         self.vel_y = 0
         self.tamanho = tamanho
         self.raio = self.tamanho // 2
+        self.coluna_intencao = int(self.coluna)
+        self.linha_intencao = int(self.linha)
 
     def calcular_regras(self):
-        self.coluna = self.coluna + self.vel_x
-        self.linha = self.linha + self.vel_y
+        self.coluna_intencao = self.coluna + self.vel_x
+        self.linha_intencao = self.linha + self.vel_y
         self.centro_x = int(self.coluna * self.tamanho + self.raio)
         self.centro_y = int(self.linha * self.tamanho + self.raio)
-
-        if self.centro_x + self.raio >= 600 or self.centro_x - self.raio <= 0:
-            self.vel_x = 0
-        if self.centro_y + self.raio > 600 or self.centro_y - self.raio <= 0:
-            self.vel_y = 0
 
     def pintar(self, tela):
         pygame.draw.circle(tela, AMARELO, (self.centro_x, self.centro_y), self.raio, 0)
@@ -93,8 +107,6 @@ class Pacman:
 
     def processar_eventos(self, eventos):
         for e in eventos:
-            if e.type == pygame.QUIT:
-                exit()
             if e.type == pygame.KEYDOWN:
                 if e.key == pygame.K_RIGHT:
                     if self.centro_x + self.raio >= 600:
@@ -117,18 +129,29 @@ class Pacman:
                     else:
                         self.vel_y = VELOCIDADE
 
+    def aceitar_movimento(self):
+        self.linha = self.linha_intencao
+        self.coluna = self.coluna_intencao
 
 if __name__ == "__main__":
     tamanho = 600 // 30
     pacman = Pacman(tamanho)
-    cenario = Cenario(tamanho)
+    cenario = Cenario(tamanho, pacman)
 
     while True:
-        tela.fill(PRETO)
+
         pacman.calcular_regras()
+        cenario.calcular_regras()
+
+        tela.fill(PRETO)
+
         cenario.pintar(tela)
         pacman.pintar(tela)
         pygame.display.update()
+        pygame.time.delay(50)
 
         eventos = pygame.event.get()
+        for e in eventos:
+            if e.type == pygame.QUIT:
+                exit()
         pacman.processar_eventos(eventos)
